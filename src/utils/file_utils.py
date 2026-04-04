7e304b75452b174967efdd4fdb8c7f807640c857
Pachyhead <bitcoin-groove0a@icloud.com>
2026-04-05 00:31:06 +0900

    generalize get_file_iterator

--- src/utils/file_utils.py
+++ src/utils/file_utils.py
@@ -1,25 +1,19 @@
-import json
 import os
 from pathlib import Path
 
 from config import PROJECT_ROOT
 
-def get_json_file_iterator(directory_path: str=os.path.join(PROJECT_ROOT, "source")):
+def get_file_iterator(directory_path: str=os.path.join(PROJECT_ROOT, "source"), extension: str = ""):
     """
-    read the directory and get the json file iterator
+    read the directory and get the file iterator
     default directory is project/source
     """
     path = Path(directory_path)
-    for file_path in path.glob(f"*.json"):
-        with open(file_path, 'r', encoding='utf-8') as f:
-            yield json.load(f)
-
-def get_file_iterator(directory_path: str = os.path.join(PROJECT_ROOT, "source")):
-    """
-    Find files only in the current level.
-    """
-    path = Path(directory_path)
-    
-    for file_path in path.glob("*"):
-        if file_path.is_file():
-            yield file_path
+    if extension == "":
+        for file_path in path.glob("*"):
+            if file_path.is_file():
+                yield file_path
+    else:
+        for file_path in path.glob(f"*.{extension}"):
+            if file_path.is_file():
+                yield file_path
